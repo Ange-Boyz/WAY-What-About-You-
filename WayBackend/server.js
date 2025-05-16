49321700ac4bcbe1e466edc592ce34d479271e40
Gnon Déolinda Bio Bogore <139719649+Deolinda1506@users.noreply.github.com>
2025-05-16 23:33:45 +0000

    Setup Express server, MongoDB connection, and user registration route

---
+++ WayBackend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+connectDB();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('👋 Welcome to WayBackend API');
+});
+
+const userRoutes = require('./routes/user.routes');
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
